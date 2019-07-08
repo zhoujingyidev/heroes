0d16221b18b5532b5455d49d22ebd8fd69838b6b
zhoujingyidev <1540640311@qq.com>
2019-07-08 16:44:34 +0800

    配置 vue-router

--- src/main.js
+++ src/main.js
@@ -7,6 +7,20 @@ Vue.config.productionTip = false
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './assets/index.css'
 
+
+//加载路由模块
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+//配置路由模块
+const appRouter = new VueRouter({
+  router:[
+
+  ]
+})
+
+
 new Vue({
   render: h => h(App),
+  router: appRouter,
 }).$mount('#app')
