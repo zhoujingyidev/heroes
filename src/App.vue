3c50c9ca4b70b88c42214850e614af3993131260
zhoujingyidev <1540640311@qq.com>
2019-07-08 16:34:33 +0800

    实现 AppHeader 和 AppSidebar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">CRUD</a>
-        </div>
-      </div>
-    </nav>
+    <!-- AppHeader -->
+    <app-header></app-header>
+
     <div class="container-fluid">
       <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Reports</a></li>
-            <li><a href="#">Analytics</a></li>
-            <li><a href="#">Export</a></li>
-          </ul>
-        </div>
+        
+        <!-- AppSidebar -->
+        <app-sidebar></app-sidebar>
+
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h2 class="sub-header">Hero List</h2>
           <a class="btn btn-success" href="add.html">Add</a>
@@ -242,12 +228,16 @@
 </template>
 
 <script>
-
+//导入AppHeader组件
+import AppHeader from './components/AppHeader'
+//导入AppSidebar组件
+import AppSidebar from './components/AppSidebar'
 
 export default {
   name: 'app',
   components: {
-   
+    AppHeader,
+    AppSidebar
   }
 }
 </script>
